3fce7bd987bcfbc4ac46726a866bb36c3f94c4d3
Robbie Bowman <robbiebowman@gmail.com>
2024-05-27 19:36:29 -0400

    Change line endings, add custom shapes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,20 @@ package org.example
 
 import com.robbiebowman.CrosswordMaker
 import com.robbiebowman.WordIsolator
+import kotlin.random.Random
 
 fun main() {
-    val puzzle = CrosswordMaker().createCrossword()
+    val crosswordMaker = CrosswordMaker()
+    var puzzle = crosswordMaker.createCrossword()
+    if(puzzle != null) {
+        puzzle.forEach {
+            println(it.joinToString(" "))
+        }
+        val (across, down) = WordIsolator.getWords(puzzle)
+        println(across)
+        println(down)
+    }
+    puzzle = crosswordMaker.createCrossword()
     if(puzzle != null) {
         puzzle.forEach {
             println(it.joinToString(" "))
--- src/main/kotlin/com/robbiebowman/CrosswordMaker.kt
+++ src/main/kotlin/com/robbiebowman/CrosswordMaker.kt
@@ -1,13 +1,13 @@
 package com.robbiebowman
 
-import java.net.URI
-import java.nio.file.*
+import java.time.Instant
+import kotlin.random.Random
 
 
-class CrosswordMaker {
+class CrosswordMaker(private val rng: Random = Random(Instant.now().epochSecond)) {
 
     private val dictionary: Set<String>
-    private var fs: FileSystem? = null
+    private val shapes: Set<Array<Array<Char?>>>
 
     init {
         dictionary = getWordsFromFile("5-letter-words.txt")
@@ -15,18 +15,14 @@ class CrosswordMaker {
             .plus(getRankedWordsFromFile("3-letter-words-ranked.txt", 3))
             .plus(getRankedWordsFromFile("2-letter-words-ranked.txt", 3))
             .plus(('a'..'z').map { it.toString() })
+
+        shapes = getShapesFromFile("layouts.txt")
     }
 
     private val dictionaryLookUps = mutableMapOf<String, Boolean>()
 
     private fun initialisePuzzle(): Crossword {
-        return arrayOf(
-            arrayOf(null, null, null, null, null),
-            arrayOf(null, ' ', null, null, null),
-            arrayOf(null, ' ', ' ', null, null),
-            arrayOf(null, ' ', null, null, null),
-            arrayOf(null, null, null, null, null),
-        )
+        return shapes.random(rng)
     }
 
     fun createCrossword(initialPuzzle: Crossword = initialisePuzzle()): Crossword? {
@@ -86,7 +82,7 @@ class CrosswordMaker {
                 val solution = dfs(node, visited, depth + 1)
                 if (solution != null) return solution
             } else {
-                getContinuations(node, depth).shuffled().forEach { neighbor ->
+                getContinuations(node, depth).shuffled(rng).forEach { neighbor ->
                     val newDepth = if (neighbor[depth].any { it == null }) depth else depth + 1
                     val solution = dfs(neighbor, newVisited, newDepth)
                     if (solution != null) return solution
@@ -96,9 +92,19 @@ class CrosswordMaker {
         return null
     }
 
+    private fun getShapesFromFile(path: String): Set<Array<Array<Char?>>> {
+        val resource = this::class.java.classLoader.getResource(path)
+        val strings = resource!!.readText().split("\\n\\n".toRegex()).flatMap { s ->
+            val shape = s.split("\\n".toRegex()).map { it.map { if (it == 'x') ' ' else null } }
+            shape.map { (0..3).map { rotateNTimes(it, shape) } }
+        }.flatten().toSet()
+        val shapes = strings.map { it.map { it.toTypedArray() }.toTypedArray() }.toSet()
+        return shapes
+    }
+
     private fun getWordsFromFile(path: String): Set<String> {
         val resource = this::class.java.classLoader.getResource(path)
-        val strings =  resource!!.readText().split("\\r?\\n".toRegex()).filter { w ->
+        val strings = resource!!.readText().split("\\n".toRegex()).filter { w ->
             w.length <= 5 && w.all { it in 'a'..'z' }
         }.toSet()
         return strings
@@ -106,13 +112,34 @@ class CrosswordMaker {
 
     private fun getRankedWordsFromFile(path: String, minRank: Int): Set<String> {
         val resource = this::class.java.classLoader.getResource(path)
-        val strings =  resource!!.readText().split("\\r?\\n".toRegex()).mapNotNull {
+        val strings = resource!!.readText().split("\\n".toRegex()).mapNotNull {
             val (word, rank) = it.split(" - ")
             if (rank.toInt() < minRank) null else word
         }.filter { w ->
             w.length <= 5 && w.all { it in 'a'..'z' }
         }.toSet()
         return strings
+    }
+
+    private fun <T> rotate90(matrix: List<List<T>>): List<List<T>> {
+        val rowCount = matrix.size
+        val colCount = matrix[0].size
+        val rotated = MutableList(colCount) { MutableList(rowCount) { matrix[0][0] } }
+
+        for (i in 0 until rowCount) {
+            for (j in 0 until colCount) {
+                rotated[j][rowCount - 1 - i] = matrix[i][j]
+            }
+        }
+        return rotated
+    }
+
+    private fun <T> rotateNTimes(n: Int, matrix: List<List<T>>): List<List<T>> {
+        var mutated = matrix
+        for (i in 0..n) {
+            mutated = rotate90(mutated)
+        }
+        return mutated
     }
 
 }
